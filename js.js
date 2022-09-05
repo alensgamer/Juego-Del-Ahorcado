456aedf920879df458fa12899428a5ea7c66f9da
ALEnsGAMERL <alexgocugumo5@gmail.com>
2022-09-05 18:19:24 -0500

    Avances de laptop a escritorio

--- js.js
+++ js.js
@@ -2,13 +2,22 @@ function con(weli){
     console.log(weli)
 }
 
-
-let palabra = ['h','o','l','a'];
-con(palabra[0]);
-var stile = palabra[1];
+let palabra = ['h','o','l','a']; //se declara el array
+con(palabra[1]); //imprimimos la letra que no queremos
+palabra[1] = [" "]; //cambiamos el valor de la letra que no queremos
 
 
 document.write(palabra.join(""));
+
+window.addEventListener("keyup", function(actualizacion){
+    //con(actualizacion)
+    
+})
+
+
+
+
+
 
 
 
@@ -16,7 +25,8 @@ window.addEventListener("keydown",function (tecla){
     con(tecla.key)
     let precionada = tecla.key;
     if (precionada == palabra [1]){
-        document.write("precionaste o")
+        palabra[1] = ["o"];
+        
     }
 })
 
